2567205c083e0d7b9feb290a3c4f06713a05d596
HKingKits <101977830+HKingKits@users.noreply.github.com>
2022-07-10 20:03:52 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,4 @@
-function validate()
-{
-    var username=document.getElementById("username").value;
-    var password=document.getElementById("password").value;
-    if(username=="admin"&& password=="user"){
-        alert(login="successfully");
-        return false;
-    }
-    else{
-        alert(login="failled")
-    }
 
-}
 
 ///////////// sign up///////
 function signup(e) {
